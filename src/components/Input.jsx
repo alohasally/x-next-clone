d50b6e9b2abe22fd70c31d896de64034b81fe70e
alohasally <peacheeks@gmail.com>
2024-09-08 21:59:49 +0900

    Add Firebase storage and complete file upload fc

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,71 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { useEffect, useRef, useState } from "react";
 import { HiOutlinePhotograph } from "react-icons/hi";
+
+import { app } from "../firebase";
+
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const Input = () => {
   const { data: session } = useSession();
+  const imageInputRef = useRef(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageUploading, setImageUploading] = useState(false);
+
+  const addImageToPost = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+      console.log(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (selectedFile) {
+      setImageUploading(true);
+      uploadImageToStorage();
+    }
+  }, [selectedFile]);
+
+  const uploadImageToStorage = () => {
+    setImageUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + "-" + selectedFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`Upload is ${progress}% done`);
+        if (snapshot.state === "success") {
+          console.log("Upload succeeded");
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            setImageFileUrl(downloadURL);
+            setImageUploading(false);
+          });
+        }
+      },
+      (error) => {
+        console.error("Upload failed", error);
+        setImageUploading(false);
+        setImageFileUrl(null);
+        setSelectedFile(null);
+      }
+    );
+  };
+
   if (!session) return null;
   return (
     <div className="w-full flex gap-4 p-2 border-b">
@@ -19,8 +80,25 @@ const Input = () => {
           rows="2"
           className="resize-y p-2 border-b w-full placeholder-gray-400 placeholder:text-xl placeholder:font-semibold"
         ></textarea>
-        <div className="flex justify-between">
-          <HiOutlinePhotograph />
+        {imageFileUrl && (
+          <img
+            src={imageFileUrl}
+            alt="image file"
+            className="w-full max-h-[250px] object-cover cursor-pointer"
+          />
+        )}
+        <div className="flex items-center justify-between">
+          <HiOutlinePhotograph
+            className="h-10 w-10 p-2 cursor-pointer text-sky-500 hover:brightness-95 hover:bg-sky-200 transition duration-200 rounded-full "
+            onClick={() => imageInputRef.current.click()}
+          />
+          <input
+            type="file"
+            ref={imageInputRef}
+            accept="image/*"
+            className="hidden"
+            onChange={addImageToPost}
+          />
           <button className="bg-blue-300 px-2 py-1 text-white font-bold rounded-xl">
             Post
           </button>
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "x-next-31984.firebaseapp.com",
+  projectId: "x-next-31984",
+  storageBucket: "x-next-31984.appspot.com",
+  messagingSenderId: "611992862270",
+  appId: "1:611992862270:web:3ed5411c67ffb2bfef9b6f",
+  measurementId: "G-QQ4DTT55MN",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
